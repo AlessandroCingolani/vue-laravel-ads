d93e65a660b98e1317ed46f6ac8cefd38b3bc0d5
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-21 16:13:54 +0100

    [del}: import scss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import '../scss/main.scss'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
