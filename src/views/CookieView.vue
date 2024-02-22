d0ab212583e2caaad4ae152f2138ed3d876ee2b5
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-22 13:33:13 +0100

    [del]:axios call

--- src/views/CookieView.vue
+++ src/views/CookieView.vue
@@ -1,20 +1,9 @@
 <script>
-import axios from "axios";
 import AdsView from "../components/partials/AdsView.vue";
 export default {
   name: "HomeView",
   components: {
     AdsView,
-  },
-  methods: {
-    getAds() {
-      axios.get("http://127.0.0.1:8000/api/ads").then((response) => {
-        console.log(response.data.ads);
-      });
-    },
-  },
-  mounted() {
-    this.getAds();
   },
 };
 </script>
