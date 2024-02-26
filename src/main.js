6eb5d1640bedf42f59f9b99481c3194427f7fd89
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-26 15:31:43 +0100

    [add]:default value axios

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-// import axios from 'axios';
+import axios from 'axios';
 
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 
 createApp(App).use(router).mount('#app')
